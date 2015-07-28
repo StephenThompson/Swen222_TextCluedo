89a66c2eb7be79dda3c2edcac70dc0e6c46dcd54
thompsstep2 <thompsstep2@steamboat.ecs.vuw.ac.nz>
2015-07-28 12:00:23 +1200

    dice

--- Swen222_TextCluedo/src/gameOfCluedo/Dice.java
+++ Swen222_TextCluedo/src/gameOfCluedo/Dice.java
@@ -1,6 +1,14 @@
 package gameOfCluedo;
-
+/**
+ * This class represents a dice
+ * @author Stephen Thompson
+ *
+ */
 public class Dice {
+	/**
+	 * Returns a randomly generated number between 1 and 6
+	 * @return int
+	 */
 	static int roll(){
 		return (int)(1 + Math.random()*6);
 	}
